4442926763b7b265f0eeaf9144dc1fe934a3b2a3
garytabker <GeeBake@outlook.com>
2020-09-26 15:51:56 -0400

    made colects collectable

--- Baker_RollABall/Assets/Scripts/PlayerController.cs
+++ Baker_RollABall/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,9 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
+
     private Rigidbody rb;
+
     private float movementX;
     private float movementY;
 
@@ -16,16 +18,26 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    void OnMove(InputValue movementValue)
+    private void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
+
         movementX = movementVector.x;
         movementY = movementVector.y;
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-        rb.AddForce(movement*speed);
+
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+        }
     }
 }
