b008d6d995758962fa0506ed3cc57e91f62d57d5
garytabker <GeeBake@outlook.com>
2020-10-05 18:26:57 -0400

    working on obstacle script

--- Baker_RollABall/Assets/Scripts/ObstacleMovement.cs
+++ Baker_RollABall/Assets/Scripts/ObstacleMovement.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class ObstacleMovement : MonoBehaviour
 {
+    bool goingLeft = true;
+    private Vector3 startPosition;
+    public float speed = .1f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (goingLeft)
+        {
+            if (transform.position.x > startPosition.x - 3)
+            {
+                transform.position.x = transform.position.x - speed;
+            }
+                    }
     }
 }
