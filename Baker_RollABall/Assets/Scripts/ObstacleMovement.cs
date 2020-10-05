1d8728f2c5e325f6c7fc536a8623ee5b447fee69
garytabker <GeeBake@outlook.com>
2020-10-05 18:37:50 -0400

    first iteration of the movement for obstacle

--- Baker_RollABall/Assets/Scripts/ObstacleMovement.cs
+++ Baker_RollABall/Assets/Scripts/ObstacleMovement.cs
@@ -6,22 +6,21 @@ public class ObstacleMovement : MonoBehaviour
 {
     bool goingLeft = true;
     private Vector3 startPosition;
+    public Vector3 endPosition;
     public float speed = .1f;
+    public Vector3 change = new Vector3(10f,0f,0f);
     // Start is called before the first frame update
     void Start()
     {
         startPosition = transform.position;
+        endPosition = startPosition + change;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (goingLeft)
-        {
-            if (transform.position.x > startPosition.x - 3)
-            {
-                transform.position.x = transform.position.x - speed;
-            }
-                    }
+        float movement = Mathf.PingPong(Time.time * speed, 1f);
+
+        transform.position = Vector3.Lerp(startPosition, endPosition, movement);
     }
 }
